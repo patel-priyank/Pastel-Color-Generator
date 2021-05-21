41f83a138795f723110c530c227ea33225a78eab
Priyank Patel <patel23priyank@gmail.com>
2021-05-21 12:39:46 +0530

    add undo button

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const body = document.querySelector('#body');
 const btnRandom = document.querySelector('#btn-random');
 const btnCopy = document.querySelector('#btn-copy');
+const btnUndo = document.querySelector('#btn-undo');
 const bgColor = document.querySelector('#bg-color');
 const bgColorTextbox = document.querySelector('#bg-color-textbox');
 const github = document.querySelector('#github');
 
-const getRandomColor = () => {
+var previousColor = '';
+
+const getRandomColorInRgb = () => {
   const hex = 'BCDEF';
   let color = '#';
 
@@ -16,10 +19,10 @@ const getRandomColor = () => {
   return color;
 };
 
-const rgbToHex = () => {
-  var colorValues = body.style.background
+const rgbToHex = (rgbColor) => {
+  var colorValues = rgbColor
     .substring(4)
-    .substring(0, body.style.background.substring(4).length - 1)
+    .substring(0, rgbColor.substring(4).length - 1)
     .split(',');
 
   var hexColor =
@@ -32,12 +35,15 @@ const rgbToHex = () => {
 };
 
 const setBackground = () => {
-  body.style.background = getRandomColor();
-  bgColor.textContent = rgbToHex();
+  previousColor = body.style.background;
+  btnUndo.disabled = false;
+
+  body.style.background = getRandomColorInRgb();
+  bgColor.textContent = rgbToHex(body.style.background);
 };
 
 const copyBackground = () => {
-  bgColorTextbox.value = rgbToHex();
+  bgColorTextbox.value = rgbToHex(body.style.background);
   bgColorTextbox.select();
   document.execCommand('copy');
   bgColorTextbox.value = '';
@@ -55,15 +61,26 @@ const copyBackground = () => {
   }, 1000);
 };
 
+const browseCode = () => {
+  window.open('https://github.com/patel-priyank/Pastel-Color-Generator');
+};
+
+const setPreviousBackground = () => {
+  body.style.background = previousColor;
+  bgColor.textContent = rgbToHex(body.style.background);
+
+  previousColor = '';
+  btnUndo.disabled = true;
+};
+
 btnRandom.addEventListener('click', setBackground);
 btnCopy.addEventListener('click', copyBackground);
-
-github.addEventListener('click', () => {
-  window.open('https://github.com/patel-priyank/Pastel-Color-Generator');
-});
+btnUndo.addEventListener('click', setPreviousBackground);
+github.addEventListener('click', browseCode);
 
 bgColorTextbox.addEventListener('keydown', (event) => {
   event.preventDefault();
 });
 
-setBackground();
+body.style.background = getRandomColorInRgb();
+bgColor.textContent = rgbToHex(body.style.background);
